b11288c704d3bd5f3371ac4340a2ea18fe4b789e
Gonzalo C <lcontreraslf@gmail.com>
2024-09-26 15:08:10 -0300

    Hito1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from "./components/Footer"
+import Home from "./components/Home"
+import Navbar from "./components/Navbar"
+
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  )
+}
+
+export default App
